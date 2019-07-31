16d1c1883aa5fa64e329941b6a9644514c700704
Adil <adelmusse@gmail.com>
2019-07-31 17:38:36 +0800

    Authentication, Devise,OAuth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
-class RegistrationsController < ApplicationController
-  def signin
-  end
+class RegistrationsController < Devise::RegistrationsController
+  protected
 
-  def signup
+  def after_sign_up_path_for(resource)
+    users_home_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root to: "home#home_page"
+  # devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   get 'users_home', to: 'users_home#users_home'
   get 'date_time', to: 'date_and_time#date_time'
   get 'signin', to: "registrations#signin"
   get 'signup', to: "registrations#signup"
-  get "home_page", to: "home#home_page"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
