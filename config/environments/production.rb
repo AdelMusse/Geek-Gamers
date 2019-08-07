83a29fcfd40181f750e1ac4700330c8fffa08dc5
Adil <adelmusse@gmail.com>
2019-08-07 15:43:08 +0800

    remove www from railgun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.credentials.railsgun_api,
-    domain: 'www.adilmusse.site'
+    domain: 'adilmusse.site'
   }
   # config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
