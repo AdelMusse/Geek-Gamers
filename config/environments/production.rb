dd492c0501faebef8c243b1f89b63e387bafc157
Adil <adelmusse@gmail.com>
2019-08-07 14:29:40 +0800

    using mailgun instead of smtp (account verified)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.credentials.railsgun_api,
     domain: 'www.adilmusse.site'
   }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
