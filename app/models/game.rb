dd35ec11486ba72e6e37655a82bdfa08ec5d2d06
Adil <adelmusse@gmail.com>
2019-08-16 11:41:19 +0800

    commenting searchkick

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,11 +2,11 @@ class Game < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_one_attached :picture
-    searchkick word_middle: [:game_name, :description]
-    def search_data
-        {
-          game_name: game_name,
-          description: description,
-        }
-      end
+    # searchkick word_middle: [:game_name, :description]
+    # def search_data
+    #     {
+    #       game_name: game_name,
+    #       description: description,
+    #     }
+    #   end
 end
